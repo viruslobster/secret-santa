4d16149048184de7313dd54d544c6430379d24e2
Michael Stevens <mls0.5mv2@gmail.com>
2025-12-26 23:03:27 -0500

    basic chat box state

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,8 @@ import (
 
 func Start(port string) error {
 	http.HandleFunc("/greeting", greetingHandler)
+	http.HandleFunc("/api/sendChat", chatHandler)
+	http.HandleFunc("/api/user", userHandler)
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 	http.HandleFunc("/", spaHandler)
 
@@ -17,10 +20,33 @@ func Start(port string) error {
 }
 
 func spaHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Got spa request")
 	http.ServeFile(w, r, "./index.html")
 }
 
 func greetingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "🎅 Ho ho ho! Merry Christmas from Santa! 🎄\n")
 	fmt.Fprintf(w, "Welcome to Santa's HTTP server!\n")
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Got chat request")
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	user := map[string]string{
+		"name": "Mr. Foo",
+		"id":   "123",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
--- www/Main.elm
+++ www/Main.elm
@@ -4,6 +4,10 @@ import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events as Events
+import Http
+import Json.Encode as Encode
+import Time
 import Url
 
 
@@ -20,17 +24,45 @@ main =
 
 
 type alias Model =
-    { key : Nav.Key, url : Url.Url }
+    { key : Nav.Key
+    , url : Url.Url
+    , user : String
+    , chats : List ChatMessage
+    , chatDraft : String
+    }
+
+
+type alias ChatMessage =
+    { message : String
+    , user : String
+    , status : ChatStatus
+    }
+
+
+type ChatStatus
+    = Unsent
+    | Sent Time.Posix
+    | Error
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url, Cmd.none )
+    ( { key = key
+      , url = url
+      , user = "Mr. Foo"
+      , chats = []
+      , chatDraft = ""
+      }
+    , Cmd.none
+    )
 
 
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
+    | SendChat ChatMessage
+    | SendChatResponse (Result Http.Error ())
+    | UpdateChatDraft String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -47,6 +79,37 @@ update msg model =
         UrlChanged url ->
             ( { model | url = url }, Cmd.none )
 
+        SendChat chat ->
+            let
+                newModel =
+                    { model | chats = model.chats ++ [ chat ], chatDraft = "" }
+
+                request =
+                    Http.post
+                        { url = "/api/sendChat"
+                        , body = Http.jsonBody (encodeChat chat)
+                        , expect = Http.expectWhatever SendChatResponse
+                        }
+            in
+            ( newModel, request )
+
+        SendChatResponse resp ->
+            case resp of
+                -- TODO: update the unsent chat message to sent
+                Ok () ->
+                    ( model, Cmd.none )
+
+                -- TODO: retry or add an error to the chat
+                Err error ->
+                    let
+                        _ =
+                            Debug.log "Chat send error" error
+                    in
+                    ( model, Cmd.none )
+
+        UpdateChatDraft draft ->
+            ( { model | chatDraft = draft }, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
@@ -55,11 +118,11 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Secret Santa", body = body model.url }
+    { title = "Secret Santa", body = body model }
 
 
-body : Url.Url -> List (Html Msg)
-body url =
+body : Model -> List (Html Msg)
+body model =
     [ div [ class "jumbotron" ]
         [ h1 [] [ text "🎅 Secret Santa 🎄" ]
         , a [ href "/group" ] [ text "Group Chat" ]
@@ -68,18 +131,57 @@ body url =
         , text " - "
         , a [ href "/giftee" ] [ text "Your Giftee Chat" ]
         ]
-    , div []
-        [ case url.path of
-            "/group" ->
-                text "we in the group"
-
-            "/secret-santa" ->
-                text "keep it secret, keep it safe"
-
-            "/giftee" ->
-                text "gimmie"
-
-            _ ->
-                text "404"
-        ]
+    , div [] [ chatView model ]
     ]
+
+
+chatView : Model -> Html Msg
+chatView model =
+    case model.url.path of
+        "/group" ->
+            chatThread model
+
+        "/secret-santa" ->
+            text "keep it secret, keep it safe"
+
+        "/giftee" ->
+            text "gimmie"
+
+        "/" ->
+            text ""
+
+        _ ->
+            text "404"
+
+
+chatThread : Model -> Html Msg
+chatThread model =
+    div [ class "chat-thread" ]
+        [ div [] (List.map chatMessage model.chats)
+        , textarea
+            [ value model.chatDraft, Events.onInput UpdateChatDraft ]
+            []
+        , button
+            [ Events.onClick
+                (SendChat
+                    { message = model.chatDraft
+                    , user = model.user
+                    , status = Unsent
+                    }
+                )
+            ]
+            [ text "Send" ]
+        ]
+
+
+chatMessage : ChatMessage -> Html Msg
+chatMessage chat =
+    div [ class "chat-message" ] [ text (chat.user ++ ": " ++ chat.message) ]
+
+
+encodeChat : ChatMessage -> Encode.Value
+encodeChat chat =
+    Encode.object
+        [ ( "message", Encode.string chat.message )
+        , ( "user", Encode.string chat.user )
+        ]
